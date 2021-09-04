8c503b05367387f9690dd044180aac79c8a5dfec
somesh202 <77766761+somesh202@users.noreply.github.com>
2021-09-05 01:03:04 +0530

    Final

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
-const connectDB = require("./config/db");
 const bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname, "public")));
-connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.get("/", (req, res) => {
@@ -19,9 +17,5 @@ app.get("/login", (req, res) => {
 app.get("/dashboard", (req, res) => {
   res.sendFile(__dirname + "/public/dashboard.html");
 });
-// app.post("/projects", (req, res) => {
-//   let { name, description, link } = req.body;
-//   res.sendFile(__dirname + "/public/projects.html");
-// })
 
 app.listen("3000", console.log("Listening on port 3000."));
